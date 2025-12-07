ae4e05d922b734110fc2c2a24e1fb184808e6097
HasinIshrakK <hasinishrakk@gmail.com>
2025-12-08 01:56:06 +0600

    Mongodb added

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.rwnir9j.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
 app.get("/", (req, res) => {
     res.send("Happy Server!");
 });
+
+async function run() {
+    try {
+        await client.connect();
+
+    } finally {
+    }
+}
+run().catch(console.dir);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
