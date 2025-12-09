dc9e6957ef3947e54ff619072059105871908793
HasinIshrakK <hasinishrakk@gmail.com>
2025-12-09 22:53:15 +0600

    User sent to DB

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.rwnir9j.mongodb.net/?appName=Cluster0`;
+
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -20,24 +21,23 @@ const client = new MongoClient(uri, {
 
 const databaseName = "ScholarStreamDB";
 const scholarshipsCollection = "scholarships";
+const usersCollection = "users";
 
 app.get("/", (req, res) => {
     res.send("ScholarStream Server is running!");
 });
 
-
-
 async function run() {
     try {
         await client.connect();
-
         const db = client.db(databaseName);
-        await db.createCollection(scholarshipsCollection);
+
+        const Scholarships = db.collection(scholarshipsCollection);
+        const Users = db.collection(usersCollection);
 
         app.get("/scholarships", async (req, res) => {
             try {
-                const db = client.db(databaseName);
-                const scholarships = await db.collection(scholarshipsCollection).find({}).toArray();
+                const scholarships = await Scholarships.find({}).toArray();
                 res.json(scholarships);
             } catch (err) {
                 console.error(err);
@@ -50,24 +50,41 @@ async function run() {
                 const id = req.params.id;
 
                 if (!ObjectId.isValid(id)) {
-                    return res.status(400).json({ error: "Invalid ID format" });
+                    return res.status(400).json({ error: "Invalid scholarship ID" });
                 }
 
-                const result = await db.collection(scholarshipsCollection).findOne({ _id: new ObjectId(id) });
+                const result = await Scholarships.findOne({ _id: new ObjectId(id) });
 
                 if (!result) {
                     return res.status(404).json({ error: "Scholarship not found" });
                 }
 
                 res.json(result);
-
             } catch (err) {
                 console.error(err);
                 res.status(500).json({ error: "Failed to fetch scholarship" });
             }
         });
 
-        console.log("Connected to MongoDB successfully.");
+        app.post("/users", async (req, res) => {
+            try {
+                const user = req.body;
+
+                const exist = await Users.findOne({ email: user.email });
+
+                if (!exist) {
+                    await Users.insertOne(user);
+                    return res.send({ success: true, message: "User saved" });
+                }
+
+                res.send({ success: true, message: "User already exists" });
+            } catch (err) {
+                console.error(err);
+                res.status(500).send({ success: false, error: err.message });
+            }
+        });
+
+        console.log("MongoDB connected successfully.");
     } catch (err) {
         console.error("MongoDB connection error:", err);
     }
@@ -75,4 +92,3 @@ async function run() {
 run().catch(console.dir);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
