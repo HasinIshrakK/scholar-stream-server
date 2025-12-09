c8cc969058740f61858a3c64216e1b46d6255ba2
HasinIshrakK <hasinishrakk@gmail.com>
2025-12-10 00:49:12 +0600

    Reviews added

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ const client = new MongoClient(uri, {
 const databaseName = "ScholarStreamDB";
 const scholarshipsCollection = "scholarships";
 const usersCollection = "users";
+const reviewsCollection = "reviews";
 
 app.get("/", (req, res) => {
     res.send("ScholarStream Server is running!");
@@ -34,6 +35,7 @@ async function run() {
 
         const Scholarships = db.collection(scholarshipsCollection);
         const Users = db.collection(usersCollection);
+        const Reviews = db.collection(reviewsCollection);
 
         app.get("/scholarships", async (req, res) => {
             try {
@@ -63,6 +65,53 @@ async function run() {
             } catch (err) {
                 console.error(err);
                 res.status(500).json({ error: "Failed to fetch scholarship" });
+            }
+        });
+
+        app.get('/reviews', async (req, res) => {
+
+            try {
+                const email = req.query.email;
+                const query = {}
+                if (email) {
+                    query.userEmail = email;
+                }
+
+                const cursor = Reviews.find(query);
+                const result = await cursor.toArray();
+                res.send(result);
+            } catch (err) {
+                res.status(500).send({ message: "Server Error" });
+            }
+        });
+
+        app.get("/reviews/:scholarshipId", async (req, res) => {
+            try {
+                const { scholarshipId } = req.params;
+                const reviews = await Reviews.find({ scholarshipId }).toArray();
+                res.json(reviews);
+            } catch (err) {
+                console.error(err);
+                res.status(500).json({ error: "Failed to fetch reviews" });
+            }
+        });
+
+        app.post("/many", async (req, res) => {
+            try {
+                const reviews = req.body;
+                if (!Array.isArray(reviews) || reviews.length === 0) {
+                    return res.status(400).json({ message: "Provide an array of reviews" });
+                }
+
+                const insertedReviews = await Reviews.insertMany(reviews);
+                res.status(201).json({
+                    message: "Reviews inserted successfully",
+                    count: insertedReviews.length,
+                    data: insertedReviews
+                });
+            } catch (err) {
+                console.error(err);
+                res.status(500).json({ message: "Server error", error: err.message });
             }
         });
 
